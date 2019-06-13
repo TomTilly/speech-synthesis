1571836e3e100a3dcc5148577aaa105d80991455
Tom Tilly <tomtillistrand@gmail.com>
2019-06-13 16:04:33 -0400

    set voice on select input change

--- js/main.js
+++ js/main.js
@@ -5,17 +5,24 @@ const pitchEl = document.getElementById('pitch');
 const textarea = document.querySelector('textarea');
 const stopButton = document.querySelector('.stop');
 const startButton = document.querySelector('.start');
+let voices;
 
 utterance.text = textarea.value;
 
 function populateVoices() {
-  const voices = this.getVoices();
+  voices = this.getVoices();
   voiceSelectEl.innerHTML = voices
-    .map(voice => `<option value="${voice.name}>${voice.name} (${voice.lang})</option>`)
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
     .join('');
 }
 
+function setVoice() {
+  utterance.voice = voices.find(voice => this.value === voice.name);
+}
+
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+voiceSelectEl.addEventListener('change', setVoice);
 
 startButton.addEventListener('click', (e) => {
   e.preventDefault();
