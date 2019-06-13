665aa4328cef9dda2c875f698314672a24efcb97
Tom Tilly <tomtillistrand@gmail.com>
2019-06-12 22:06:10 -0400

    add start and stop button functionality

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ const pitchEl = document.getElementById('pitch');
 const textarea = document.querySelector('textarea');
 const stopButton = document.querySelector('.stop');
 const startButton = document.querySelector('.start');
-let voices = [];
 
 utterance.text = textarea.value;
 
 function populateVoices() {
-  voices = this.getVoices();
+  const voices = this.getVoices();
   voiceSelectEl.innerHTML = voices
     .map(voice => `<option value="${voice.name}>${voice.name} (${voice.lang})</option>`)
     .join('');
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+startButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  speechSynthesis.speak(utterance);
+});
+
+stopButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  speechSynthesis.cancel();
+});
